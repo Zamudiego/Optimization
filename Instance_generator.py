30a42e512f844cef993fad02f82286e84b8a3028
Zamudiego <79379695+Zamudiego@users.noreply.github.com>
2023-07-04 14:37:36 -0600

    Update Instance_generator.py

--- Instance_generator.py
+++ Instance_generator.py
@@ -1,5 +1,15 @@
 import numpy as np
 
+def polinomio(x,parameters):
+  power=len(parameters-1)
+  while power>0:
+    
+  
 
-def instance():
-  return 0
+
+def instance(n,power):
+  x1=np.random.randint(-1000,1000,n)
+  parameters=np.random.randint(-1000,1000,power+1)
+  y1=polinomio(x1,parameters)
+  r=[x1,y1,parameters]
+  return r
